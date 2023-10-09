8114f17777682a387abace35273c90e7dd747991
Jaime <jaimedapontealgaba@gmail.com>
2023-10-09 18:36:09 +0200

    Segundo commit, con la función de cambiar de ventana

--- VariosActivitysConDatos/app/src/main/java/com/example/variosactivivtycondatos/MainActivity.kt
+++ VariosActivitysConDatos/app/src/main/java/com/example/variosactivivtycondatos/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.variosactivivtycondatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivivtycondatos.databinding.ActivityMainBinding
+import com.example.variosactivivtycondatos.databinding.ActivityVentana2Binding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.boton.setOnClickListener {
+
+            cambiarVentana()
+
+
+        }
     }
+
+    private fun cambiarVentana() {
+       // lateinit var binding: ActivityVentana2Binding
+
+        //binding=ActivityVentana2Binding.inflate(layoutInflater)
+        //setContentView(binding.root)
+        var miIntent: Intent =Intent(this, Ventana2::class.java)
+        startActivity(miIntent)
+
+    }
+
+
 }
